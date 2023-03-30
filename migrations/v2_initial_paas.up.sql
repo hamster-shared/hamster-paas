c4681114fddf20f8bf2cffeb78b012a2cd14a72b
sunjianguo <1275596256@qq.com>
2023-03-30 17:32:19 +0800

    add request template data

--- migrations/v2_initial_paas.up.sql
+++ migrations/v2_initial_paas.up.sql
@@ -39,11 +39,11 @@ CREATE TABLE IF NOT EXISTS t_cl_consumer (
 DROP TABLE IF EXISTS t_cl_request_template;
 CREATE TABLE IF NOT EXISTS t_cl_request_template (
     id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     created datetime NOT NULL,
     script text NOT NULL,
     author VARCHAR(20) NOT NULL,
-    description VARCHAR(255) NOT NULL
+    description VARCHAR(500) NOT NULL
     );
 
 DROP TABLE IF EXISTS t_cl_request;
---
+++ migrations/v3_add_template_data.down.sql
@@ -0,0 +1,1 @@
+delete from t_cl_request_template where id in (1,2,3,4,5,6,7,8,9,10,11,12,13,14);
