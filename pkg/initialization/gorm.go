f5b9dcc8aef99f88fbb7b3855bcf6c94e6e8e54e
jlvihv <vihv@qq.com>
2023-04-10 20:08:46 +0800

    update docker script

--- pkg/initialization/gorm.go
+++ pkg/initialization/gorm.go
@@ -11,7 +11,13 @@ import (
 )
 
 func InitDB() {
-	db, err := gorm.Open(mysql.Open(os.Getenv("MYSQL_DSN")), &gorm.Config{
+	user := "root"
+	password := os.Getenv("MYSQL_ROOT_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	dbName := os.Getenv("MYSQL_DATABASE")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, dbName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
 			SingularTable: true,
 			TablePrefix:   "t_cl_",
