3118ae2a766b090f29356504911bd9f993e6c9e0
mohaijiang <mohaijiang110@163.com>
2023-11-16 14:09:14 +0800

    modify auth to jwt

--- pkg/handler/auth_handler.go
+++ pkg/handler/auth_handler.go
@@ -6,7 +6,6 @@ import (
 	"hamster-paas/pkg/application"
 	"hamster-paas/pkg/consts"
 	"hamster-paas/pkg/rpc/aline"
-	"hamster-paas/pkg/utils"
 	"log"
 	"net/http"
 	"os"
@@ -14,38 +13,6 @@ import (
 )
 
 func (h *HandlerServer) Authorize() gin.HandlerFunc {
-	return func(gin *gin.Context) {
-		accessToken := gin.Request.Header.Get("Access-Token")
-		if accessToken == "" {
-			Failed(http.StatusUnauthorized, "access not authorized", gin)
-			gin.Abort()
-			return
-		}
-		token := utils.AesDecrypt(accessToken, consts.SecretKey)
-		userService, err := application.GetBean[*aline.UserService]("userService")
-		if err != nil {
-			Failed(http.StatusUnauthorized, "get user failed", gin)
-			return
-		}
-		user, err := userService.GetUserByToken(token)
-		if err != nil {
-			Failed(http.StatusUnauthorized, err.Error(), gin)
-			gin.Abort()
-			return
-		}
-		if user.Token == "" {
-			Failed(http.StatusUnauthorized, "access not authorized", gin)
-			gin.Abort()
-			return
-		}
-		user.Token = accessToken
-		gin.Set("token", token)
-		gin.Set("user", user)
-		gin.Next()
-	}
-}
-
-func (h *HandlerServer) JwtAuthorize() gin.HandlerFunc {
 	return func(gin *gin.Context) {
 		jwtToken := gin.GetHeader("Authorization")
 		log.Println(jwtToken)
